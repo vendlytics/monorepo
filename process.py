d248511ad493f021f4577cf798d8ce881c401417
cberkay <c.berkayantmen@gmail.com>
2018-06-24 22:36:28 -0400

    Using ray origin (bbox center) for poi calculation.

--- process.py
+++ process.py
@@ -6,9 +6,19 @@
 # - CSV with column per product at each frame
 # with the confidence that you are looking at each product
 
-import pandas as pd
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from collections import namedtuple
 import numpy as np
+import os
+import pandas as pd
 from scipy.spatial.distance import euclidean
+
+USE_BBOX = False
+
+Ray = namedtuple('Ray', ['origin', 'direction'])
 
 def calculate_rectangle_centroid(min_x, min_y, max_x, max_y):
     """
@@ -36,30 +46,29 @@ def convert_from_euler_angle_to_vector(yaw_angle, pitch_angle):
         np.sin(pitch_angle_radians)
     ])
 
-def calculate_poi(n_vector, i_vector, dist_estimate):
+def calculate_poi(n_vector, ray, dist_estimate):
     """
     Finds the point of intersection of a vector on the shelf plane.
     
     n_vector is shelf plane vector i.e. normal to the shelf plane
-    i_vector is intersecting vector
+    ray is the combination of origin and direction to intersect with the plane
     dist_estimate is estimate of shelf distance from person
     
     Notation from http://www.ambrsoft.com/TrigoCalc/Plan3D/PlaneLineIntersection_.htm
     """
     p_vector = n_vector + dist_estimate
-    t = -(np.dot(n_vector, p_vector) + dist_estimate) / float(np.dot(n_vector, i_vector))  
-    return i_vector * t
+    t = -(np.dot(n_vector, p_vector) + dist_estimate) / float(np.dot(n_vector, ray.direction))
+    return ray.origin + ray.direction * t
 
 def get_abs_euclidean_distance(vec_1, vec_2):
     return np.absolute(euclidean(vec_1, vec_2))
 
 
 class Pipeline:
-    def __init__(self, name, inputs, output_path):
-        self.name = name
-        
+    def __init__(self, inputs, output_path):
         # INPUTS
         self.raw_gaze_path = inputs['raw_gaze_path']
+        self.bbox_path = inputs['bbox_path']
         self.products_on_shelf_plane_coords_path = inputs['products_on_shelf_plane_coords_path']
         self.shelf_plane_norm_path = inputs['shelf_plane_norm_path']
         self.dist_estimate_path = inputs['dist_estimate_path']
@@ -74,32 +83,52 @@ class Pipeline:
         return gaze_df
 
     # LOAD STAGE 2
+    def ingest_bboxes(self):
+        bbox_df = pd.read_csv(self.bbox_path, delimiter=' ', header=None)
+        bbox_df.columns = ['frame_number', 'x_min', 'y_min', 'x_max', 'y_max', 'confidence_score']
+        return bbox_df
+
+    # LOAD STAGE 3
     def ingest_products_on_shelf_plane_coords(self):
         return np.loadtxt(self.products_on_shelf_plane_coords_path, delimiter=' ')
 
-    # LOAD STAGE 3
+    # LOAD STAGE 4
     def ingest_shelf_plane_norm(self):
         return np.loadtxt(self.shelf_plane_norm_path, delimiter=' ')
 
-    # LOAD STAGE 4
+    # LOAD STAGE 5
     def ingest_dist_estimate(self):
         return np.loadtxt(self.dist_estimate_path)
 
-    def compute_distance_scores_for_products(self, gaze_df, product_plane_coords, 
+    def compute_distance_scores_for_products(self, gaze_df, bbox_df, product_plane_coords,
                                              plane_n_vector, shelf_dist_estimate):
+        # TODO: Choose mean of calibration
+        calibration_gaze_origin = np.array([616.0491945, 126.5688365, 0])
 
         for i in range(len(product_plane_coords)):
             gaze_df['product {}'.format(i)] = None
 
+        assert set(gaze_df.index) == set(bbox_df.index), \
+            'Gaze and bounding box data should cover the same frames'
+
         # 1. for each gaze vector, get the point of intersection on the plane
         for idx in gaze_df.index:
-            print(idx)
             gaze_x_angle = gaze_df.iloc[idx]['x_angle']
             gaze_y_angle = gaze_df.iloc[idx]['y_angle']
-            gaze_z_angle = gaze_df.iloc[idx]['z_angle']
-            
             z_vector = convert_from_euler_angle_to_vector(gaze_x_angle, gaze_y_angle)
-            poi = calculate_poi(plane_n_vector, z_vector, shelf_dist_estimate)
+
+            if USE_BBOX:
+                bbox_coords = bbox_df.iloc[idx][['x_min', 'y_min', 'x_max', 'y_max']].values
+                bbox_center = calculate_rectangle_centroid(*bbox_coords)
+                unnormalized_gaze_origin = np.array([bbox_center[0], bbox_center[1], 0])
+                gaze_origin = unnormalized_gaze_origin - calibration_gaze_origin
+            else:
+                gaze_origin = np.zeros(3)
+
+            poi = calculate_poi(
+                plane_n_vector,
+                Ray(origin=gaze_origin, direction=z_vector),
+                shelf_dist_estimate)
 
             # 2. calculate for each product the distance between the product's vector and POI on plane
             for i in range(len(product_plane_coords)):
@@ -113,43 +142,46 @@ class Pipeline:
         return True
 
     def run(self):
-        gaze_df = self.ingest_raw_gaze()[:5000]
+        num_frames = 12000
+        gaze_df = self.ingest_raw_gaze()[:num_frames]
+        bbox_df = self.ingest_bboxes()[:num_frames]
         product_plane_coords = self.ingest_products_on_shelf_plane_coords()
-        print(product_plane_coords)
         shelf_plane_norm = self.ingest_shelf_plane_norm()
         dist_estimate = self.ingest_dist_estimate()
 
-        new_gaze_df = self.compute_distance_scores_for_products(gaze_df, product_plane_coords, 
+        new_gaze_df = self.compute_distance_scores_for_products(gaze_df, bbox_df,
+                                                                product_plane_coords, 
                                                                 shelf_plane_norm, dist_estimate)
         
         if self.write_results_per_frame(new_gaze_df):
-            print("DONE")
+            print("DONE: {} frames".format(new_gaze_df.shape[0]))
 
 
 if __name__ == "__main__":
-    RAW_GAZE_PATH = "notebooks/visualization/data/frame_to_gaze.txt"
+    RAW_GAZE_PATH = "notebooks/visualization/notebook_data/frame_to_gaze.txt"
     
     # not used
-    BBOX_PATH = "notebooks/visualization/data/frame_to_bbox.txt"
+    BBOX_PATH = "notebooks/visualization/notebook_data/frame_to_bbox.txt"
 
     # the coordinates of products on shelf
-    PRODUCTS_ON_SHELF_PLANE_COORDS_PATH = "notebooks/visualization/data/product_plane_coords.txt"
+    PRODUCTS_ON_SHELF_PLANE_COORDS_PATH = "notebooks/visualization/notebook_data/product_plane_coords.txt"
     
     # norm of the shelf plane
-    SHELF_PLANE_NORM_PATH = "notebooks/visualization/data/shelf_plane_norm.txt" 
+    SHELF_PLANE_NORM_PATH = "notebooks/visualization/notebook_data/shelf_plane_norm.txt"
     
     # the gaze vectors for the products (not used)
-    PRODUCT_GAZE_VECTORS_PATH = "notebooks/visualization/data/product_vectors.txt"
+    PRODUCT_GAZE_VECTORS_PATH = "notebooks/visualization/notebook_data/product_vectors.txt"
     
     # the set distance of calibrated face from shelf
-    DIST_ESTIMATE_PATH = "notebooks/visualization/data/dist_estimate.txt"
+    DIST_ESTIMATE_PATH = "notebooks/visualization/notebook_data/dist_estimate.txt"
     
-    OUTPUT_PATH = "notebooks/visualization/data/output.txt"
+    filename = "output_with_bbox.txt" if USE_BBOX else "output_without_bbx.txt"
+    OUTPUT_PATH = os.path.join("notebooks/visualization/notebook_data/", filename)
 
     pipeline = Pipeline(
-        'fucked pipeline',
         inputs={
             'raw_gaze_path': RAW_GAZE_PATH,
+            'bbox_path': BBOX_PATH,
             'products_on_shelf_plane_coords_path': PRODUCTS_ON_SHELF_PLANE_COORDS_PATH,
             'shelf_plane_norm_path': SHELF_PLANE_NORM_PATH,
             'dist_estimate_path': DIST_ESTIMATE_PATH
