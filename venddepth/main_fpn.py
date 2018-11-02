[Fixes] Minus sign to poi in process.py
- also makes sure that we can handle frames in which faces are not
detected - the gaze code now writes a line for the frame but without
any angles instead of totally skipping recording anything for that
frame.

venddepth/main_fpn.py
@@ -6,8 +6,8 @@ import argparse, time
 from utils.net_utils import adjust_learning_rate
 import torch
 from torch.autograd import Variable
-# from dataset.dataloader import DepthDataset
-from dataset.nyuv2_dataset import NYUv2Dataset
+# from scripts.dataloader import DepthDataset
+from scripts.nyuv2_dataset import NYUv2Dataset
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import transforms
@@ -123,7 +123,7 @@ def parse_args():
                       default='nyuv2', type=str)
     parser.add_argument('--epochs', dest='max_epochs',
                       help='number of epochs to train',
-                      default=NUM_EPOCHS, type=int)
+                      default=40, type=int)
     parser.add_argument('--cuda', dest='cuda',
                       help='whether use CUDA',
                       action='store_true')

vendflow/overlay_prediction_v2.py
@@ -7,25 +7,27 @@ import os
 from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip, concatenate, \
     clips_array
 
-preds_path = 'smooth_prediction.npy'
-video1_path = 'gaze_overlay_24fps.mp4'
-video4_path = 'Camera 4 - Vendlytics Prototype.mp4'
+root_path = 'notebooks/data_for_demo_v2/'
+preds_path = root_path + 'smooth_predictions.npy'
+video1_path = root_path + 'vendgaze/output-test.avi'
+video4_path = root_path + 'petstore-demo-rear.mp4'
 
 fps = 24
-offset = 28.4
+offset = 0
 start_frame = 0
 end_frame = 12000
 start_sec = offset + start_frame / fps
 end_sec = offset + end_frame / fps
 
 idx_to_name = [
-    "Hershey's Reese",
-    "Twix Bites",
-    "Snickers Bites",
-    "Sensations Rice",
-    "Uncle Ben's Rice",
-    "Purina Dog Treats",
-    "Pedigree Dog Treats",
+    "product 0",
+    "product 1",
+    "product 2",
+    "product 3",
+    "product 4",
+    "product 5",
+    "product 6",
+    "product 7"
 ]
 
 do_downscale = False
@@ -33,8 +35,13 @@ out_filename = "overlay_downscaled_with_gaze.mp4" if do_downscale else "overlay_
 
 
 def pred_to_text_clip(pred):
-    text = idx_to_name[int(pred)]
-    return TextClip(text, fontsize=30, color='white').set_duration(1 / fps)
+    text = "No Prediction"
+
+    if not np.isnan(pred):
+        text = idx_to_name[int(pred)]
+
+    return TextClip(text, fontsize=50, bg_color='white', 
+                    color='black', stroke_width=3).set_duration(1 / fps)
 
 
 def delete_file_if_exists(filename):
@@ -53,15 +60,18 @@ def get_video(path, with_audio):
 
 preds = np.load(preds_path)[start_frame:end_frame]
 video1 = get_video(video1_path, with_audio=True)
+video4 = get_video(video4_path, with_audio=False)
 
 stacked_video = clips_array([[video1, video4]])
 
 text_clip = concatenate([pred_to_text_clip(pred)
                          for pred in preds], method="compose")
 
+print("creating compositevideoclip")
 result = CompositeVideoClip([stacked_video, text_clip])
 
 # so we don't get permission issues when writing
 delete_file_if_exists(out_filename)
 
+print("writing video out to: {}".format(out_filename))
 result.write_videofile(out_filename, fps=fps, codec='mpeg4')

vendflow/process.py
@@ -3,7 +3,7 @@
 # - CSV product calibration file
 #
 # outputs:
-# - CSV with column per product at each frame
+# - CSV with column of product being looked at for each frame
 # with the confidence that you are looking at each product
 # and prediction + smoothed prediction
 
@@ -13,6 +13,8 @@ from __future__ import print_function
 
 from collections import namedtuple
 import numpy as np
+# np.set_printoptions(threshold=np.nan)
+
 import os
 import pandas as pd
 from scipy.signal import medfilt
@@ -22,20 +24,6 @@ from scipy.spatial.distance import euclidean
 USE_BBOX = False
 
 Ray = namedtuple('Ray', ['origin', 'direction'])
-
-
-def calculate_rectangle_centroid(min_x, min_y, max_x, max_y):
-    """
-    Returns a tuple (x, y) which gives the position of the face bounding box.
-    """
-    return (min_x + max_x) / 2, (min_y + max_y) / 2
-
-
-def calculate_rectangle_area(min_x, min_y, max_x, max_y):
-    """
-    Returns the pixel area of the face bounding box.
-    """
-    return (max_x - min_x) * (max_y - min_y)
 
 
 def convert_from_euler_angle_to_vector(yaw_angle, pitch_angle):
@@ -73,21 +61,21 @@ def get_abs_euclidean_distance(vec_1, vec_2):
 
 
 class Pipeline:
-    def __init__(self, inputs, output_path):
+    def __init__(self, inputs, outputs):
         # INPUTS
-        self.num_products = inputs['num_products']
         self.raw_gaze_path = inputs['raw_gaze_path']
         self.products_on_shelf_plane_coords_path = inputs['products_on_shelf_plane_coords_path']
         self.shelf_plane_norm_path = inputs['shelf_plane_norm_path']
         self.dist_estimate_path = inputs['dist_estimate_path']
 
         # OUTPUT
-        self.output_path = output_path
+        self.raw_output_path = outputs['raw_output_path']
+        self.smooth_predictions_path = outputs['smooth_predictions_path']
 
     # LOAD STAGE 1
     def ingest_raw_gaze(self):
         gaze_df = pd.read_csv(self.raw_gaze_path, delimiter=' ', header=None)
-        gaze_df.columns = ['frame_number', 'x_angle', 'y_angle', 'z_angle']
+        gaze_df.columns = ['frame_number', 'x_angle', 'y_angle', 'z_angle', 'x_center', 'y_center']
         return gaze_df
 
     # LOAD STAGE 3
@@ -106,7 +94,6 @@ class Pipeline:
 
     def compute_distance_scores_for_products(
             self,
-            num_products,
             gaze_df,
             product_plane_coords,
             plane_n_vector,
@@ -116,17 +103,26 @@ class Pipeline:
         # calibration_gaze_origin = np.array([616.0491945, 126.5688365, 0])
 
         for i in range(len(product_plane_coords)):
-            print(i)
             gaze_df['product {}'.format(i)] = None
 
         # 1. for each gaze vector, get the point of intersection on the plane
         for idx in gaze_df.index:
+            print(idx)
+            if np.isnan(gaze_df.iloc[idx]['x_angle']):
+                for i in range(len(product_plane_coords)):
+                    gaze_df.at[idx, 'product {}'.format(i)] = np.nan
+                gaze_df.at[idx, 'predictions'] = np.nan
+                gaze_df.at[idx, 'smooth_predictions'] = np.nan
+                continue
+
             gaze_x_angle = gaze_df.iloc[idx]['x_angle']
             gaze_y_angle = gaze_df.iloc[idx]['y_angle']
             z_vector = convert_from_euler_angle_to_vector(
                 gaze_x_angle, gaze_y_angle)
             
-            gaze_origin = np.zeros(3)
+            gaze_origin = np.array(
+                    [gaze_df.iloc[idx]['x_center'], gaze_df.iloc[idx]['y_center'], 0])
+            # gaze_origin = np.zeros(3)
 
             poi = calculate_poi(
                 plane_n_vector,
@@ -137,70 +133,62 @@ class Pipeline:
             # vector and POI on plane
             for i in range(len(product_plane_coords)):
                 gaze_df.at[idx, 'product {}'.format(
-                    i)] = get_abs_euclidean_distance(-poi, product_plane_coords[i])
+                    i)] = get_abs_euclidean_distance(poi, product_plane_coords[i])
+
+            product_cols = ['product {}'.format(i) for i in range(len(product_plane_coords))]
+            gaze_df.at[idx, 'predictions'] = np.argmin(gaze_df.iloc[idx][product_cols])[0]
 
         # 3. make regular and smoothed predictions based on product distances
-        product_cols = ['product {}'.format(i) for i in range(num_products)]
-        distances = gaze_df[product_cols].values  # num_frames x num_products
-        gaze_df['predictions'] = np.argmin(distances, axis=1)  # num_frames
-        gaze_df['smoothed_predictions'] = medfilt(
+        gaze_df['smooth_predictions'] = medfilt(
             gaze_df['predictions'], kernel_size=89)
 
         return gaze_df
 
+    def write_smooth_predictions(self, smooth_predictions):
+        np.save(self.smooth_predictions_path, smooth_predictions)
+        return True
+
     def write_results_per_frame(self, new_gaze_df):
-        # write out to self.output_path
-        new_gaze_df.to_csv(self.output_path)
+        new_gaze_df.to_csv(self.raw_output_path)
         return True
 
     def run(self):
-        # the assumption here being that the raw video for calibration is 
-        # in clean state (no extra frames)
-
         gaze_df = self.ingest_raw_gaze()
         product_plane_coords = self.ingest_products_on_shelf_plane_coords()
         shelf_plane_norm = self.ingest_shelf_plane_norm()
         dist_estimate = self.ingest_dist_estimate()
 
         new_gaze_df = self.compute_distance_scores_for_products(
-            self.num_products, gaze_df, product_plane_coords, shelf_plane_norm, dist_estimate)
+            gaze_df, product_plane_coords, shelf_plane_norm, dist_estimate)
 
-        if self.write_results_per_frame(new_gaze_df):
+        if self.write_results_per_frame(new_gaze_df) and \
+            self.write_smooth_predictions(new_gaze_df['smooth_predictions'].values):
             print("DONE: {} frames".format(new_gaze_df.shape[0]))
 
 
 if __name__ == "__main__":
 
-    NUM_PRODUCTS = 11
-
     RAW_GAZE_PATH = "notebooks/data_for_demo_v2/vendgaze/output-test.txt"
-
-    # not used
-    BBOX_PATH = "notebooks/data_for_demo_v2/vendnet/video-det-fold-None.txt"
-
-    # the coordinates of products on shelf
     PRODUCTS_ON_SHELF_PLANE_COORDS_PATH = "notebooks/data_for_demo_v2/product_plane_coords.txt"
-
-    # norm of the shelf plane
     SHELF_PLANE_NORM_PATH = "notebooks/data_for_demo_v2/shelf_plane_norm.txt"
-
-    # the gaze vectors for the products (not used)
-    PRODUCT_GAZE_VECTORS_PATH = "notebooks/data_for_demo_v2/product_vectors.txt"
-
-    # the set distance of calibrated face from shelf
     DIST_ESTIMATE_PATH = "notebooks/data_for_demo_v2/dist_estimate.txt"
 
-    filename = "output_with_bbox.txt" if USE_BBOX else "output_without_bbx.txt"
-    OUTPUT_PATH = os.path.join(
-        "notebooks/data_for_demo_v2/", filename)
+    RAW_OUTPUT_PATH = os.path.join(
+        "notebooks/data_for_demo_v2/", "output_without_bbx.txt")
+
+    SMOOTH_PREDICTIONS_PATH = os.path.join(
+        "notebooks/data_for_demo_v2/", "smooth_predictions.npy")
 
     pipeline = Pipeline(
         inputs={
             'raw_gaze_path': RAW_GAZE_PATH,
-            'num_products': NUM_PRODUCTS,
             'products_on_shelf_plane_coords_path': PRODUCTS_ON_SHELF_PLANE_COORDS_PATH,
             'shelf_plane_norm_path': SHELF_PLANE_NORM_PATH,
-            'dist_estimate_path': DIST_ESTIMATE_PATH},
-        output_path=OUTPUT_PATH)
+            'dist_estimate_path': DIST_ESTIMATE_PATH
+        },
+        outputs={
+            'raw_output_path': RAW_OUTPUT_PATH,
+            'smooth_predictions_path': SMOOTH_PREDICTIONS_PATH
+        })
 
     pipeline.run()

vendgaze/code/test_on_video_dockerface.py
@@ -175,6 +175,7 @@ if __name__ == '__main__':
             break
         cv2_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+        no_dets_for_cur_frame = True
         while True:
             x_min, y_min, x_max, y_max, conf = int(
                 float(
@@ -188,6 +189,8 @@ if __name__ == '__main__':
                 line[5])
 
             if conf >= 0.9995:
+                no_dets_for_cur_frame = False
+
                 bbox_width = abs(x_max - x_min)
                 bbox_height = abs(y_max - y_min)
                 # x_min -= 3 * bbox_width / 4
@@ -227,12 +230,15 @@ if __name__ == '__main__':
                     pitch_predicted.data[0] * idx_tensor) * 3 - 99
                 roll_predicted = torch.sum(
                     roll_predicted.data[0] * idx_tensor) * 3 - 99
+                
+                x_center = (x_min + x_max) / 2
+                y_center = (y_min + y_max) / 2
 
                 # Print new frame with cube and axis
                 txt_out.write(
-                    str(frame_num) + ' %f %f %f\n' %
-                    (yaw_predicted, pitch_predicted, roll_predicted))
-                # utils.plot_pose_cube(frame, yaw_predicted, pitch_predicted, roll_predicted, (x_min + x_max) / 2, (y_min + y_max) / 2, size = bbox_width)
+                    str(frame_num) + ' %f %f %f %f %f\n' %
+                    (yaw_predicted, pitch_predicted, roll_predicted, x_center, y_center))
+                # utils.plot_pose_cube(frame, yaw_predicted, pitch_predicted, roll_predicted, x_center, y_center, size = bbox_width)
                 utils.draw_axis(
                     frame,
                     yaw_predicted,
@@ -250,6 +256,11 @@ if __name__ == '__main__':
             # Peek next frame detection
             next_frame_num = int(
                 bbox_line_list[idx + 1].strip('\n').split(SEPARATOR)[0])
+            
+            if next_frame_num > frame_num and no_dets_for_cur_frame:
+                print("no dets for frame:")
+                txt_out.write(str(frame_num) + ' NaN NaN NaN NaN NaN\n')
+
             # print 'next_frame_num ', next_frame_num
             if next_frame_num == det_frame_num:
                 idx += 1